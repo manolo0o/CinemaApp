fb5205ac58fbcda8339aa89f7bad4c084cab9533
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-07-27 20:43:51 -0500

    feat: :white_check_mark: Find movies by id and diff parameters
    Creation of method getMoviesByID where u can change the parameters to find of a specific movie, only putting his id
    
    none at moment

--- js/module/casesManagement/managementMovies.js
+++ js/module/casesManagement/managementMovies.js
@@ -40,8 +40,60 @@ class movies extends connect {
      * @returns {Promise<Array<Equipo>>} - Array con todos los resultados de los Noticias.
      */
 
-    async getAllmovies(){
-        let res = await this.collection.find({}).toArray();
+    async getAllmovies(){ // method to find movies, and project its title,id,gender and duration.
+        let res = await this.collection.find(
+            {},
+            {
+                projection:{
+                    "titulo": 1,
+                    "genero": 1,
+                    "duracion": 1 
+                }
+            }
+        ).toArray();
+        return res;
+    }
+//____________________ MOVIES BY ID ____________________________
+
+    /**
+     * @typedef {Object} movies
+     * @property {string} idMovies - Movie id.
+     * @property {string} titulo - movie title.
+     * @property {string} descripcion - description of the movie.
+     * @property {string} director - Movie director.
+     * @property {string} actores - Actor from the movie.
+     * @property {string} genero - Film gender.
+     * @property {string} duracion - Film duration.
+     * @property {string} fecha_estreno - Dremiere date.
+     * @property {string} calificacion - criticism.
+     * @property {string} idioma - lenguage of the movie.
+     * @property {string} subtitulos - Available captions.
+     * @property {string} formato - movie format (2D,3D,IMAX...ETC).
+     */
+
+    /**
+     * Obtiene todos los resultados de la colección.
+     * @returns {Promise<Array<Equipo>>} - Array con todos los resultados de los Noticias.
+     */
+    async getMoviesByID(idMovies){
+        let res = await this.collection.find(
+            {"_id": new ObjectId(idMovies)},
+            {
+                projection:{
+                    "titulo": 1,
+                    "descripcion": 1,
+                    "director": 1,
+                    "actores": 1,
+                    "genero": 1,
+                    "duracion": 1,
+                    "fecha_estreno": 1,
+                    "calificacion": 1,
+                    "idioma": 1,
+                    "subtitulos": 1,
+                    "formato": 1
+                }
+            }
+        ).toArray();
         return res;
     }
 }
--- js/module/runMovies.js
+++ js/module/runMovies.js
@@ -11,7 +11,24 @@ export const getAllmovies =  async () => {
         console.log("Movies:", movie);
         return movie;
     } catch (error){
-        console.error("Error obteining all players", error);
+        console.error("Error obtaining all players", error);
+        throw error;;
+    } finally {
+        await moviesInstance.close();
+    }
+};
+
+//____________________ MOVIES BY ID ____________________________
+
+export const getMoviesByID =  async (idMovies) => {
+    let moviesInstance = new movies();
+    await moviesInstance.initialize();
+    try{
+        const movie = await moviesInstance.getMoviesByID(idMovies);
+        console.log("Movies:", movie);
+        return movie;
+    } catch (error){
+        console.error("Error obtaining all players", error);
         throw error;;
     } finally {
         await moviesInstance.close();
@@ -21,6 +38,12 @@ export const getAllmovies =  async () => {
 (async() => {
 //____________________ ALL MOVIES ____________________________
 
-await getAllmovies();
+//await getAllmovies();
+
+//____________________ MOVIES BY ID ____________________________
+
+// Parameter to find the movie
+    let idMovies = "64b28b5c3f8b9e6fdbc7febe"; // insert the id you wanna find
+    await getMoviesByID(idMovies); // call the method
 
 })();
