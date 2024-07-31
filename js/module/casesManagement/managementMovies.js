de458548387ea72788f9e5cbf5fcc55f25669093
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-07-31 10:09:57 -0500

    feat: :memo: Last documentation for movie files.

--- js/module/casesManagement/managementMovies.js
+++ js/module/casesManagement/managementMovies.js
@@ -15,71 +15,57 @@ class movies extends connect {
 
     async initialize(){
         await this.open();
-        this.collection = this.db.collection("peliculas")    
+        this.collection = this.db.collection("peliculas");
     }
-//____________________ ALL MOVIES ____________________________
+
+    //____________________ ALL MOVIES ____________________________
 
     /**
      * @typedef {Object} movies
-     * @property {string} _id - Movie id.
-     * @property {string} titulo - movie title.
-     * @property {string} descripcion - description of the movie.
-     * @property {string} director - Movie director.
-     * @property {string} actores - Actor from the movie.
-     * @property {string} genero - Film gender.
-     * @property {string} duracion - Film duration.
-     * @property {string} fecha_estreno - Dremiere date.
-     * @property {string} calificacion - criticism.
-     * @property {string} idioma - lenguage of the movie.
-     * @property {string} subtitulos - Available captions.
-     * @property {string} formato - movie format (2D,3D,IMAX...ETC).
+     * @property {string} _id - ID de la película.
+     * @property {string} titulo - Título de la película.
+     * @property {string} descripcion - Descripción de la película.
+     * @property {string} director - Director de la película.
+     * @property {string} actores - Actores de la película.
+     * @property {string} genero - Género de la película.
+     * @property {string} duracion - Duración de la película.
+     * @property {string} fecha_estreno - Fecha de estreno de la película.
+     * @property {string} calificacion - Calificación de la película.
+     * @property {string} idioma - Idioma de la película.
+     * @property {string} subtitulos - Subtítulos disponibles para la película.
+     * @property {string} formato - Formato de la película (2D, 3D, IMAX, etc.).
      */
 
     /**
-     * get the result of the collection.
-     * @returns {Promise<Array<movies>>} - Array with the result of movies collection.
+     * Obtiene todos los documentos de la colección de películas, proyectando solo el título, ID, género y duración.
+     * @returns {Promise<Array<movies>>} - Array con el resultado de la colección de películas.
      */
-
-    async getAllmovies(){ // method to find movies, and project its title,id,gender and duration.
+    async getAllmovies() {
         let res = await this.collection.find(
             {},
             {
-                projection:{
+                projection: {
                     "titulo": 1,
                     "genero": 1,
-                    "duracion": 1 
+                    "duracion": 1
                 }
             }
         ).toArray();
         return res;
     }
-//____________________ MOVIES BY ID ____________________________
+
+    //____________________ MOVIES BY ID ____________________________
 
     /**
-     * @typedef {Object} movies
-     * @property {string} idMovies - Movie id.
-     * @property {string} titulo - movie title.
-     * @property {string} descripcion - description of the movie.
-     * @property {string} director - Movie director.
-     * @property {string} actores - Actor from the movie.
-     * @property {string} genero - Film gender.
-     * @property {string} duracion - Film duration.
-     * @property {string} fecha_estreno - Dremiere date.
-     * @property {string} calificacion - criticism.
-     * @property {string} idioma - lenguage of the movie.
-     * @property {string} subtitulos - Available captions.
-     * @property {string} formato - movie format (2D,3D,IMAX...ETC).
+     * Obtiene los detalles completos de una película por su ID.
+     * @param {string} idMovies - ID de la película.
+     * @returns {Promise<Array<movies>>} - Array con el resultado de la película solicitada.
      */
-
-    /**
-     * Obtiene todos los resultados de la colección.
-     * @returns {Promise<Array<Equipo>>} - Array con todos los resultados de los Noticias.
-     */
-    async getMoviesByID(idMovies){
+    async getMoviesByID(idMovies) {
         let res = await this.collection.find(
             {"_id": new ObjectId(idMovies)},
             {
-                projection:{
+                projection: {
                     "titulo": 1,
                     "descripcion": 1,
                     "director": 1,
@@ -96,51 +82,37 @@ class movies extends connect {
         ).toArray();
         return res;
     }
-//____________________ FUNCTIONS BY MOVIEID ____________________________
-    
-    /**
-     * @typedef {Object} movies
-     * @property {string} idMovies - Movie id.
-     * @property {string} titulo - movie title.
-     * @property {string} descripcion - description of the movie.
-     * @property {string} director - Movie director.
-     * @property {string} actores - Actor from the movie.
-     * @property {string} genero - Film gender.
-     * @property {string} duracion - Film duration.
-     * @property {string} fecha_estreno - Dremiere date.
-     * @property {string} calificacion - criticism.
-     * @property {string} idioma - lenguage of the movie.
-     * @property {string} subtitulos - Available captions.
-     * @property {string} formato - movie format (2D,3D,IMAX...ETC).
-     */
+
+    //____________________ FUNCTIONS BY MOVIEID ____________________________
 
     /**
-     * Obtiene todos los resultados de la colección.
-     * @returns {Promise<Array<Equipo>>} - Array con todos los resultados de los Noticias.
+     * Obtiene las funciones asociadas a una película por su ID, incluyendo la disponibilidad de asientos y horarios.
+     * @param {string} idMovies - ID de la película.
+     * @returns {Promise<Array<Object>>} - Array con el resultado de las funciones asociadas a la película.
      */
-    async getMovieFunctionsByID(idMovies){
+    async getMovieFunctionsByID(idMovies) {
         let res = await this.collection.aggregate([
-                { $match: {_id: new ObjectId(idMovies) } },
-                {
-                    $lookup: {
-                        from: "funciones",
-                        localField: "_id",
-                        foreignField: "pelicula_id",
-                        as: "funciones"
+            { $match: {_id: new ObjectId(idMovies)} },
+            {
+                $lookup: {
+                    from: "funciones",
+                    localField: "_id",
+                    foreignField: "pelicula_id",
+                    as: "funciones"
                 }
-                },
-                {
-                    $project:{
-                        '_id':0,
-                        'titulo':1,
-                        'funciones.asientos_disponibles':1,
-                        'funciones.asientos_totales':1,
-                        'funciones.horas':1
-                    }
+            },
+            {
+                $project: {
+                    '_id': 0,
+                    'titulo': 1,
+                    'funciones.asientos_disponibles': 1,
+                    'funciones.asientos_totales': 1,
+                    'funciones.horas': 1
                 }
-            ]).toArray();
-            return res;
-        }
-        
+            }
+        ]).toArray();
+        return res;
+    }
 }
+
 export default movies;
--- js/module/runMovies.js
+++ js/module/runMovies.js
@@ -1,18 +1,23 @@
 import { ObjectId } from "mongodb";
-import movies from "./casesManagement/managementMovies.js"
+import movies from "./casesManagement/managementMovies.js";
 
 //____________________ ALL MOVIES ____________________________
 
-export const getAllmovies =  async () => {
+/**
+ * Obtiene todos los documentos de la colección de películas y los muestra en la consola.
+ * @returns {Promise<Array<Object>>} - Array con el resultado de la colección de películas.
+ * @throws {Error} - Lanza un error si ocurre algún problema al obtener las películas.
+ */
+export const getAllmovies = async () => {
     let moviesInstance = new movies();
     await moviesInstance.initialize();
-    try{
+    try {
         const movie = await moviesInstance.getAllmovies();
         console.log("Movies:", movie);
         return movie;
-    } catch (error){
+    } catch (error) {
         console.error("Error obtaining all movies...", error);
-        throw error;;
+        throw error;
     } finally {
         await moviesInstance.close();
     }
@@ -20,16 +25,22 @@ export const getAllmovies =  async () => {
 
 //____________________ MOVIES BY ID ____________________________
 
-export const getMoviesByID =  async (idMovies) => {
+/**
+ * Obtiene los detalles completos de una película por su ID.
+ * @param {string} idMovies - ID de la película que se desea obtener.
+ * @returns {Promise<Array<Object>>} - Array con el resultado de la película solicitada.
+ * @throws {Error} - Lanza un error si ocurre algún problema al obtener la película por ID.
+ */
+export const getMoviesByID = async (idMovies) => {
     let moviesInstance = new movies();
     await moviesInstance.initialize();
-    try{
+    try {
         const movie = await moviesInstance.getMoviesByID(idMovies);
         console.log("Movies:", movie);
         return movie;
-    } catch (error){
+    } catch (error) {
         console.error("Error obtaining the movies by id...", error);
-        throw error;;
+        throw error;
     } finally {
         await moviesInstance.close();
     }
@@ -37,37 +48,43 @@ export const getMoviesByID =  async (idMovies) => {
 
 //____________________ FUNCTIONS BY MOVIEID ____________________________
 
-export const getMovieFunctionsByID =  async (idMovies) => {
+/**
+ * Obtiene las funciones asociadas a una película por su ID, incluyendo la disponibilidad de asientos y horarios.
+ * @param {string} idMovies - ID de la película para la que se desean obtener las funciones.
+ * @returns {Promise<Array<Object>>} - Array con el resultado de las funciones asociadas a la película.
+ * @throws {Error} - Lanza un error si ocurre algún problema al obtener las funciones por ID de película.
+ */
+export const getMovieFunctionsByID = async (idMovies) => {
     let moviesInstance = new movies();
     await moviesInstance.initialize();
-    try{
+    try {
         const movie = await moviesInstance.getMovieFunctionsByID(idMovies);
         console.log("Movies:", JSON.stringify(movie, null, 2));
         return movie;
-    } catch (error){
+    } catch (error) {
         console.error("Error obtaining the functions by movie ID...", error);
-        throw error;;
+        throw error;
     } finally {
         await moviesInstance.close();
     }
 };
 
+// Ejecuta las funciones para pruebas
+(async () => {
+    //____________________ ALL MOVIES ____________________________
 
+    // Obtiene todas las películas
+    // await getAllmovies();
 
-(async() => {
-//____________________ ALL MOVIES ____________________________
+    //____________________ MOVIES BY ID ____________________________
 
-//await getAllmovies();
+    // Parámetro para encontrar la película
+    // let idMovies = "64b28b5c3f8b9e6fdbc7febe"; // Inserta el ID que deseas encontrar
+    // await getMoviesByID(idMovies); // Llama al método
 
-//____________________ MOVIES BY ID ____________________________
+    //____________________ FUNCTIONS BY MOVIEID ____________________________
 
-// Parameter to find the movie
-//    let idMovies = "64b28b5c3f8b9e6fdbc7febe"; // insert the id you wanna find
-//    await getMoviesByID(idMovies); // call the method
-
-//____________________ FUNCTIONS BY MOVIEID ____________________________
-
- let idMovies = "64b28b5c3f8b9e6fdbc7febe";
- await getMovieFunctionsByID(idMovies);
+    let idMovies = "64b28b5c3f8b9e6fdbc7febe";
+    await getMovieFunctionsByID(idMovies);
 
 })();
