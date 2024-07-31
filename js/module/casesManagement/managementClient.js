376c556bd98214763baeb7eeebc36d82ac82be57
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-07-31 09:57:53 -0500

    feat: :memo: Adding documentation for clents.

--- js/module/casesManagement/managementClient.js
+++ js/module/casesManagement/managementClient.js
@@ -1,110 +1,105 @@
 import { ObjectId } from "mongodb";
 import { connect } from "../../../helpers/db/connect.js";
 
-class clients extends connect{
+/**
+ * Clase `clients` que maneja las operaciones de la colección `clientes` en MongoDB.
+ * Se extiende de la clase `connect` para gestionar la conexión a la base de datos.
+ */
+class clients extends connect {
     static instance;
 
-    constructor(){
+    /**
+     * Constructor de la clase `clients`.
+     * Implementa el patrón Singleton para asegurar que solo haya una instancia de la clase.
+     */
+    constructor() {
         super();
-        if(typeof clients.instance === "object"){
+        if (typeof clients.instance === "object") {
             return clients.instance;
         }
         clients.instance = this;
         return this;
     }
-    async initialize(){
+
+    /**
+     * Método para inicializar la conexión a la base de datos y obtener la colección `clientes`.
+     * @returns {Promise<void>}
+     */
+    async initialize() {
         await this.open();
-        this.collection = this.db.collection("clientes")
+        this.collection = this.db.collection("clientes");
     }
 
-// ______________________ ALL CLIENTS ________________________
+    // ______________________ ALL CLIENTS ________________________
 
-/**
-     * @typedef {Object} clients
-     * @property {string} idClient - clients id.
-     * @property {string} name - client name .
-     * @property {string} email - client email.
-     * @property {string} telefono - client telephone .
-     * @property {string} direccion - client adress.
-     * @property {string} tipo_cliente_id - client type ID.
-     * @property {string}  reservas - client bookings.
-     */
     /**
-     * get the result of the collection.
-     * @returns {Promise<Array<clients>>} - Array with the result of clients.
+     * @typedef {Object} Client
+     * @property {string} idClient - ID del cliente.
+     * @property {string} name - Nombre del cliente.
+     * @property {string} email - Email del cliente.
+     * @property {string} telefono - Teléfono del cliente.
+     * @property {string} direccion - Dirección del cliente.
+     * @property {string} tipo_cliente_id - ID del tipo de cliente.
+     * @property {string} reservas - Reservas del cliente.
      */
 
-    async getAllClients(){
-        let res = await this.collection.find(
-            {}
-        ).toArray();
+    /**
+     * Obtiene todos los clientes de la colección.
+     * @returns {Promise<Array<Client>>} - Array con todos los clientes.
+     */
+    async getAllClients() {
+        let res = await this.collection.find({}).toArray();
         return res;
-    } 
+    }
 
-// ______________________ CLIENT SATATUS BY ID ________________________
+    // ______________________ CLIENT STATUS BY ID ________________________
 
-/**
-     * @typedef {Object} clients
-     * @property {string} idClient - clients id.
-     * @property {string} name - client name .
-     * @property {string} email - client email.
-     * @property {string} telefono - client telephone .
-     * @property {string} direccion - client adress.
-     * @property {string} tipo_cliente_id - client type ID.
-     * @property {string}  reservas - client bookings.
-     */
     /**
-     * get the result of the collection.
-     * @returns {Promise<Array<clients>>} - Array with the result of clients.
+     * @typedef {Object} Client
+     * @property {string} idClient - ID del cliente.
+     * @property {string} name - Nombre del cliente.
+     * @property {string} email - Email del cliente.
+     * @property {string} telefono - Teléfono del cliente.
+     * @property {string} direccion - Dirección del cliente.
+     * @property {string} tipo_cliente_id - ID del tipo de cliente.
+     * @property {string} reservas - Reservas del cliente.
      */
-    async vipClients (tipo_cliente_id){
+
+    /**
+     * Obtiene clientes VIP basados en su tipo.
+     * @param {string} tipo_cliente_id - ID del tipo de cliente.
+     * @returns {Promise<Array<Client>>} - Array con los clientes VIP.
+     */
+    async vipClients(tipo_cliente_id) {
         let res = await this.collection.aggregate([
             { $match: { tipo_cliente_id: new ObjectId(tipo_cliente_id) } },
-                {
+            {
                 $lookup: {
                     from: "tipoCliente",
                     localField: "tipo_cliente_id",
                     foreignField: "_id",
                     as: "tipoClienteInfo",
-                        pipeline:[
+                    pipeline: [
                         {
-                        $project:{
-                            _id:0,
-                            nombre: 1,
-                            descuento: 1
-                        }
+                            $project: {
+                                _id: 0,
+                                nombre: 1,
+                                descuento: 1
+                            }
                         }
                     ]
                 }
-                },
-                {
+            },
+            {
                 $project: {
-                    _id:0,
-                    tipo_cliente_id:0,
-                    reservas:0
+                    _id: 0,
+                    tipo_cliente_id: 0,
+                    reservas: 0
                 }
-                }
+            }
         ]).toArray();
         return res;
     }
-
 }
+
 export default clients;
-
-
-// "salas": [
-//     {
-//       "sala_id": {
-//         "$oid": "64b28b5c3f8b9e6fdbc7fec0"
-//       },
-//       "nombre": "Sala 1",
-//       "capacidad": 150
-//     },
-//     {
-//       "sala_id": {
-//         "$oid": "64b28b5c3f8b9e6fdbc7fec1"
-//       },
-//       "nombre": "Sala 2",
-//       "capacidad": 200
-//     }
-//   ]
--- js/module/runClients.js
+++ js/module/runClients.js
@@ -1,48 +1,62 @@
 import { ObjectId } from "mongodb";
-import clients from "./casesManagement/managementClient.js"
+import clients from "./casesManagement/managementClient.js";
 
 // ______________________ ALL CLIENTS ________________________
 
-export const getAllClients =  async () => {
+/**
+ * Función para obtener todos los clientes.
+ * Crea una instancia de la clase `clients`, la inicializa y llama al método `getAllClients`.
+ * @returns {Promise<Array>} - Array con todos los clientes.
+ * @throws {Error} - Si ocurre un error al obtener los clientes.
+ */
+export const getAllClients = async () => {
     let clientInstance = new clients();
     await clientInstance.initialize();
-    try{
+    try {
         const client = await clientInstance.getAllClients();
         console.log("clients:", client);
         return client;
-    } catch (error){
+    } catch (error) {
         console.error("Error obtaining all clients...", error);
-        throw error;;
+        throw error;
     } finally {
         await clientInstance.close();
     }
 };
 
-// ______________________ CLIENT SATATUS BY ID ________________________
+// ______________________ CLIENT STATUS BY ID ________________________
 
-export const vipClients =  async (tipo_cliente_id) => {
+/**
+ * Función para obtener clientes VIP por tipo de cliente.
+ * Crea una instancia de la clase `clients`, la inicializa y llama al método `vipClients`.
+ * @param {string} tipo_cliente_id - ID del tipo de cliente.
+ * @returns {Promise<Array>} - Array con los clientes VIP.
+ * @throws {Error} - Si ocurre un error al obtener los clientes VIP.
+ */
+export const vipClients = async (tipo_cliente_id) => {
     let clientInstance = new clients();
     await clientInstance.initialize();
-    try{
+    try {
         const client = await clientInstance.vipClients(tipo_cliente_id);
         console.log("clients:", JSON.stringify(client, null, 2));
         return client;
-    } catch (error){
+    } catch (error) {
         console.error("Error obtaining client...", error);
-        throw error;;
+        throw error;
     } finally {
         await clientInstance.close();
     }
 };
+
 //_____________________________________________________________
-(async() => {
-// ______________________ ALL CLIENTS ________________________
 
-// await getAllClients();
+(async () => {
+    // ______________________ ALL CLIENTS ________________________
 
-// ______________________ CLIENT SATATUS BY ID ________________________
+    // await getAllClients();
 
-    let tipo_cliente_id = '64b28b5c3f8b9e6fdbc7febf'
+    // ______________________ CLIENT STATUS BY ID ________________________
+
+    let tipo_cliente_id = '64b28b5c3f8b9e6fdbc7febf';
     await vipClients(tipo_cliente_id);
-
 })();
